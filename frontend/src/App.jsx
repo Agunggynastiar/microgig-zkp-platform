996fabba57c1e783e13349dd01084513e9eb9fe5
Agung Gynastiar <agung.105240010@gmail.iwu.ac.id>
2026-05-12 12:40:43 +0700

    Update UI role based

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,24 @@ import {
 
 function App() {
 
+  const statusText = [
+
+    
+  "Lowongan Dibuat",
+  "Freelancer Ditugaskan",
+  "Pembayaran Disimpan",
+  "Pekerjaan Selesai",
+  "Pembayaran Dikirim"
+];
+
+const isEmployer = (job) =>
+  wallet.toLowerCase() === job.employer.toLowerCase();
+
+const isFreelancer = (job) =>
+  wallet.toLowerCase() === job.freelancer.toLowerCase();
+
+
+
   const [wallet, setWallet] = useState("");
   const [contract, setContract] = useState(null);
   const [jobs, setJobs] = useState([]);
@@ -164,35 +182,113 @@ async function completeJob(jobId) {
 
   try {
 
-    const proof = {
-      pi_a: [
-        "949656363989055572364175027612875642901366302055300338733508366485940468120",
-        "7814882767364351887011350807354751627314205868721183507979805245065289833495"
-      ],
+    let proof;
 
-      pi_b: [
+const selectedJob = jobs.find(
+  (j) => j.id == jobId
+);
+
+const skill = selectedJob.requiredSkill;
+
+if (skill == "10") {
+
+  proof = {
+    pi_a: [
+      "949656363989055572364175027612875642901366302055300338733508366485940468120",
+      "7814882767364351887011350807354751627314205868721183507979805245065289833495"
+    ],
+
+    pi_b: [
+      [
+        "5873421308627507156550710996214213903603974058827377956733110127537757299920",
+        "3731728869669522293386671209618082793026917009687922253833812871757869612955"
+      ],
+      [
+        "6814875797560047549914515432359605124901548773897203178545864738441246538175",
+        "13164653906222748058950189378625011704849335908422420717699943479961424665633"
+      ]
+    ],
+
+    pi_c: [
+      "2918177792676186671194070433094612663234275157612623257105831167932733036750",
+      "14149075242263668371026943145012502272264759947115583528177697407984598897657"
+    ],
+
+    pubSignals: [
+      "10",
+      "999",
+      "10"
+    ]
+  };
+}
+
+if (skill == "20") {
+
+  proof = {
+
+    pi_a: [
+      "21838613041428168010345377736023831960655814423969377386983214838429383521117",
+      "13862020560419530839561155215355422621320261789472981637820286301634896303891"
+    ],
+
+    pi_b: [
+      [
+        "15841283087304985220035471340679930325722483370270607909187988719240420061281",
+        "1364511635070405218771850410068562467958673722018689899546627657124142528437"
+      ],
+      [
+        "17397251756736037833475632092367982464704255509820014466047340914386018614479",
+        "15566158914198332253775096816269015463696024779252718018260541950844507028622"
+      ]
+    ],
+
+    pi_c: [
+      "15924059998488255043813396170639911456462212314628992957594399882456384920914",
+      "20166900432259659836521502732742365954179743616013297544780554926506231875613"
+    ],
+
+    pubSignals: [
+      "20",
+      "999",
+      "20"
+    ]
+  };
+}
+
+if (skill == "30") {
+  proof = {
+ "pi_a": [
+  "19776718378441959206518039935760872609845188751330897594386836174971931796504",
+  "15731415089750077834768704381577100481819828310154024118365535821829359554015",
+  "1"
+ ],
+ "pi_b": [
   [
-    "5873421308627507156550710996214213903603974058827377956733110127537757299920",
-    "3731728869669522293386671209618082793026917009687922253833812871757869612955"
+   "6082254268364376432006733851180542651058353621279391718581962521374743616010",
+   "7458566460638915718751706433809288938436715351370247696300392964097781158219"
   ],
   [
-    "6814875797560047549914515432359605124901548773897203178545864738441246538175",
-    "13164653906222748058950189378625011704849335908422420717699943479961424665633"
+   "10547162994642914639429511848276020683450964776591783059110227678850296772932",
+   "4034322392252170839222524014160658130959050522979358532146222637350610300804"
+  ],
+  [
+   "1",
+   "0"
   ]
-],
+ ],
+ "pi_c": [
+  "8262490802189872625415954310406849482984788696400641346408173427078707400389",
+  "20354325796210800086435179314987776116738160620252969453144906009958043477961",
+  "1"
+ ], 
 
-      pi_c: [
-        "2918177792676186671194070433094612663234275157612623257105831167932733036750",
-        "14149075242263668371026943145012502272264759947115583528177697407984598897657"
-      ],
 
-      pubSignals: [
-        "10",
-        "999",
-        "10"
-      ]
-    };
-
+ "pubSignals": [
+  "30",
+  "999",
+  "30"
+ ]}
+}
     const tx = await contract.completeJob(
       jobId,
       proof.pi_a,
@@ -244,9 +340,16 @@ async function releasePayment(jobId) {
 
       <div className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-xl">
 
-        <h1 className="text-3xl font-bold mb-6 text-center">
-          MicroGig ZKP Platform
-        </h1>
+        <p className="text-center text-gray-600 mb-4">
+
+  {wallet.toLowerCase() ===
+  "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266".toLowerCase()
+
+    ? "Mode Employer"
+
+    : "Mode Freelancer"}
+
+</p>
 
         <button
           onClick={connectWallet}
@@ -255,8 +358,10 @@ async function releasePayment(jobId) {
           Connect MetaMask
         </button>
 
-        {wallet && (
-
+        {wallet &&
+wallet.toLowerCase() ===
+"0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266".toLowerCase()
+&& (
           <div className="mt-6">
 
             <div className="bg-gray-100 p-4 rounded-xl mb-4">
@@ -312,44 +417,108 @@ async function releasePayment(jobId) {
         className="bg-gray-100 p-4 rounded-xl mb-3"
       >
 
-        <p><strong>ID:</strong> {job.id}</p>
+        <h3 className="text-lg font-bold mb-2">
+  Job #{job.id}
+</h3>
 
         <p><strong>Reward:</strong> {job.reward} ETH</p>
 
         <p><strong>Required Skill:</strong> {job.requiredSkill}</p>
 
-        <p><strong>Status:</strong> {job.status}</p>
+        <p className="mt-2">
 
-        <button
-  onClick={() =>
-    assignFreelancer(
-      job.id,
-      "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
-    )
-  }
-  className="bg-blue-500 text-white px-3 py-2 rounded-lg mr-2"
->
-  Assign Freelancer
-</button>
-<button
-  onClick={() => depositPayment(job.id, job.reward)}
-  className="mt-3 ml-3 bg-yellow-500 text-white px-4 py-2 rounded-xl"
->
-  Deposit Payment
-</button>
+  <strong>Status:</strong>
 
-<button
-  onClick={() => completeJob(job.id)}
-  className="bg-purple-600 text-white px-3 py-2 rounded-lg"
->
-  Complete Job (ZKP)
-</button>
-<button
-  onClick={() => releasePayment(job.id)}
-  className="bg-red-500 text-white px-4 py-2 rounded-xl"
->
-  Release Payment
-</button>
+  <span className="ml-2 font-semibold text-blue-600">
+    {statusText[job.status]}
+  </span>
+
+</p>
+
+        <div className="flex flex-wrap gap-2 mt-3">
+
+  {/* ASSIGN */}
+  {isEmployer(job) && job.status == 0 && (
+
+    <button
+      onClick={() =>
+        assignFreelancer(
+          job.id,
+          "0x70997970C51812dc3A010C7d01b50e0d17dc79C8"
+        )
+      }
+      className="bg-blue-500 text-white px-4 py-2 rounded-xl"
+    >
+      Assign Freelancer
+    </button>
+
+  )}
+
+  {/* DEPOSIT */}
+  {isEmployer(job) && job.status == 1 && (
+
+    <button
+      onClick={() => depositPayment(job.id, job.reward)}
+      className="bg-yellow-500 text-white px-4 py-2 rounded-xl"
+    >
+      Deposit Payment
+    </button>
+
+  )}
+
+  {/* COMPLETE JOB */}
+  {isFreelancer(job) && job.status == 2 && (
+
+    <button
+      onClick={() => completeJob(job.id)}
+      className="bg-purple-600 text-white px-4 py-2 rounded-xl"
+    >
+      Complete Job (ZKP)
+    </button>
+
+  )}
+
+  {/* RELEASE PAYMENT */}
+  {isEmployer(job) && job.status == 3 && (
+
+    <button
+      onClick={() => releasePayment(job.id)}
+      className="bg-red-500 text-white px-4 py-2 rounded-xl"
+    >
+      Release Payment
+    </button>
+
+  )}
+
+  {/* PAID */}
+  {job.status == 4 && (
+
+    <div className="bg-green-600 text-white px-4 py-2 rounded-xl">
+      Pembayaran Selesai
+    </div>
+
+  )}
+  {wallet &&
+wallet.toLowerCase() !==
+"0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266".toLowerCase()
+&& (
+
+  <div className="bg-white shadow-xl rounded-2xl p-6 w-full max-w-xl mt-6">
+
+    <h2 className="text-2xl font-bold mb-3">
+      Panel Freelancer
+    </h2>
+
+    <p className="text-gray-600">
+      Selesaikan pekerjaan dan kirim Zero Knowledge Proof
+      untuk memverifikasi skill Anda.
+    </p>
+
+  </div>
+
+)}
+
+</div>
 
       </div>
 
